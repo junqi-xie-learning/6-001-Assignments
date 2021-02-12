d6bfd58212db9455c33fd1035f40372691907ea7
Junqi Xie <junqi_xie@outlook.com>
2021-02-12 11:15:50 +0800

    Add Sample Project 7

--- 7 Generic Arithmetic/ps-code.scm
+++ 7 Generic Arithmetic/ps-code.scm
@@ -12,6 +12,8 @@
 (define (mul x y) (apply-generic 'mul x y))
 (define (div x y) (apply-generic 'div x y))
 
+;;;   (GN, GN) --> Bool
+(define (equ? x y) (apply-generic 'equ? x y))
 
 ;;;   GN --> GN
 (define (negate x) (apply-generic 'negate x))
@@ -25,7 +27,6 @@
 (define (square x) (mul x x))
 
 
-
 ;;; THE NUMBER PACKAGE
 
 ;;; Methods for dealing with ordinary numbers.
@@ -37,6 +38,9 @@
 (define (-number x y) (make-number (- x y)))
 (define (*number x y) (make-number (* x y)))
 (define (/number x y) (make-number (/ x y)))
+
+;;;   (RepNum, RepNum) --> Bool
+(define (=number x y) (= x y))
 
 ;;;   RepNum --> ({number} X RepNum)
 (define (negate-number x) (make-number (- x)))
@@ -55,8 +59,9 @@
 (put 'mul '(number number) *number)
 (put 'div '(number number) /number)
 
-(put 'negate '(number) negate-number)
+(put 'equ? '(number number) =number)
 
+(put 'negate '(number) negate-number)
 (put '=zero? '(number) =zero-number?)
 
 
@@ -80,6 +85,9 @@
 (define (*rational x y) (make-rational (*rat x y)))
 (define (/rational x y) (make-rational (/rat x y)))
 
+;;;   (RepRat, RepRat) --> Bool
+(define (=rational x y) (equ-rat? x y))
+
 ;;;   RepRat --> ({rational} X RepRat)
 (define (negate-rational x) (make-rational (negate-rat x)))
 
@@ -97,6 +105,8 @@
 (put 'mul '(rational rational) *rational)
 (put 'div '(rational rational) /rational)
 
+(put 'equ? '(rational rational) =rational)
+
 (put 'negate '(rational) negate-rational)
 (put '=zero? '(rational) =zero-rational?)
 
@@ -111,12 +121,9 @@
    (make-rational (make-rat x y)))
 
 
-
-
 ;;; THE RATIONAL ARITHMETIC PACKAGE.
 
 ;;;   (RepRat, RepRat) --> RepRat
-
 (define (+rat x y)
   (make-rat (add (mul (numer x) (denom y))
                  (mul (denom x) (numer y)))
@@ -135,15 +142,16 @@
   (make-rat (mul (numer x) (denom y))
             (mul (denom x) (numer y))))
 
+(define (equ-rat? x y)
+  (equ? (div (numer x) (denom x))
+        (div (numer y) (denom y))))
 
 ;;;   RepRat --> RepRat
-
 (define (negate-rat x)
   (make-rat (negate (numer x))
             (denom x)))
 
 ;;;   RepRat --> Bool
-
 (define (=zero-rat? x)
   (=zero? (numer x)))
 
@@ -161,12 +169,59 @@
 ;;; Coercion procedure from rational/rational method
 ;;; to number/rational method
 
-;;;  ((RepRat,RepRat) --> T) --> ((RepNum,RepRat) --> T)
+(define (repnum->reprat num)
+  (make-rat (create-number num) (create-number 1)))
+
+;;;  ((RepRat, RepRat) --> T) --> ((RepNum, RepRat) --> T)
 (define (RRmethod->NRmethod method)
   (lambda (num rat)
     (method
      (repnum->reprat num)
      rat)))
+
+;;;  ((RepRat, RepRat) --> T) --> ((RepRat, RepNum) --> T)
+(define (RRmethod->RNmethod method)
+  (lambda (rat num)
+    (method
+     rat
+     (repnum->reprat num))))
+
+;;; Methods for numbers and rationals.
+
+;;;   (RepNum, RepRat) --> ({rational} X RepRat)
+(define +number-rational (RRmethod->NRmethod +rational))
+(define -number-rational (RRmethod->NRmethod -rational))
+(define *number-rational (RRmethod->NRmethod *rational))
+(define /number-rational (RRmethod->NRmethod /rational))
+
+;;;   (RepRat, RepNum) --> ({rational} X RepRat)
+(define +rational-number (RRmethod->RNmethod +rational))
+(define -rational-number (RRmethod->RNmethod -rational))
+(define *rational-number (RRmethod->RNmethod *rational))
+(define /rational-number (RRmethod->RNmethod /rational))
+
+;;;   (RepNum, RepRat) --> Bool
+(define =number-rational (RRmethod->NRmethod =rational))
+
+;;;   (RepRat, RepNum) --> Bool
+(define =rational-number (RRmethod->RNmethod =rational))
+
+
+;;; Install the number/rational methods in the generic operations.
+
+(put 'add '(number rational) +number-rational)
+(put 'sub '(number rational) -number-rational)
+(put 'mul '(number rational) *number-rational)
+(put 'div '(number rational) /number-rational)
+
+(put 'add '(rational number) +rational-number)
+(put 'sub '(rational number) -rational-number)
+(put 'mul '(rational number) *rational-number)
+(put 'div '(rational number) /rational-number)
+
+(put 'equ? '(number rational) =number-rational)
+
+(put 'equ? '(rational number) =rational-number)
 
 
 ;;; THE GENERIC POLYNOMIAL PACKAGE
@@ -176,9 +231,15 @@
 (define (+polynomial p1 p2)
   (make-polynomial (+poly p1 p2)))
 
+(define (-polynomial p1 p2)
+  (make-polynomial (-poly p1 p2)))
 
 (define (*polynomial p1 p2)
   (make-polynomial (*poly p1 p2)))
+
+;;;   (RepPoly, RepPoly) --> Bool
+(define (equ-polynomial? p1 p2)
+  (equ-poly? p1 p2))
 
 ;;;   RepPoly --> ({polynomial} X RepPoly)
 (define (negate-polynomial x)
@@ -186,7 +247,6 @@
 
 ;;;   RepPoly --> Bool
 (define (=zero-polynomial? p) (=zero-poly? p))
-
 
 ;;;   RepPoly --> ({polynomial} X RepPoly)
 (define (make-polynomial poly)
@@ -196,8 +256,12 @@
 ;;; Install the polynomial methods in the generic operations.
 
 (put 'add '(polynomial polynomial) +polynomial)
+(put 'sub '(polynomial polynomial) -polynomial)
 (put 'mul '(polynomial polynomial) *polynomial)
 
+(put 'equ? '(polynomial polynomial) equ-polynomial?)
+
+(put 'negate '(polynomial) negate-polynomial)
 (put '=zero? '(polynomial) =zero-polynomial?)
 
 ;;; Polynomial Package User Interface
@@ -209,6 +273,10 @@
 ;;;    (Variable, List(GN)) --> ({polynomial} X RepPoly)
 (define (create-polynomial var coeffs)
   (make-polynomial (make-poly var (dense/coeffs->sparse/terms coeffs))))
+
+;;;    (Variable, List(RepNum)) --> ({polynomial} X RepPoly)
+(define (create-numerical-polynomial var coeffs)
+  (create-polynomial var (map create-number coeffs)))
 
 
 ;;; Makes sparse term representation out of a dense coefficient list
@@ -235,24 +303,37 @@
 (define (+poly p1 p2)
   (if (same-variable? (variable p1) (variable p2))
       (make-poly (variable p1)
-     (+terms (term-list p1)
-       (term-list p2)))
+                 (+terms (term-list p1)
+                         (term-list p2)))
       (error "Polys not in same var -- +POLY"
-       (list p1 p2))))
+             (list p1 p2))))
 
-;;; Need -poly ****
+(define (-poly p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (make-poly (variable p1)
+                 (-terms (term-list p1)
+                         (term-list p2)))
+      (error "Polys not in same var -- -POLY"
+             (list p1 p2))))
 
 (define (*poly p1 p2)
   (if (same-variable? (variable p1) (variable p2))
       (make-poly (variable p1)
-     (*terms (term-list p1)
-       (term-list p2)))
+                 (*terms (term-list p1)
+                         (term-list p2)))
       (error "Polys not in same var -- *POLY"
-       (list p1 p2))))
+             (list p1 p2))))
+
+;;;   (RepPoly, RepPoly) --> Bool
+(define (equ-poly? p1 p2)
+  (if (same-variable? (variable p1) (variable p2))
+      (=zero-poly? (-poly p1 p2))
+      false))
 
 ;;;   RepPoly --> RepPoly
-;;; Need negate-poly ****
-
+(define (negate-poly p)
+  (make-poly (variable p)
+             (negate-terms (term-list p))))
 
 ;;;   RepPoly --> Bool
 (define (=zero-poly? p)
@@ -277,7 +358,6 @@
 ;;; descending powers.
 
 ;;;   (RepTerms, RepTerms) --> RepTerms
-
 (define (+terms L1 L2)
   (cond ((empty-termlist? L1) L2)
         ((empty-termlist? L2) L1)
@@ -296,6 +376,9 @@
                                (+terms (rest-terms L1)
                                        (rest-terms L2)))))))))
 
+(define (-terms L1 L2)
+  (+terms L1 (negate-terms L2)))
+
 (define (*terms L1 L2)
   (if (empty-termlist? L1)
       (the-empty-termlist)
@@ -307,6 +390,12 @@
 (define (*-term-by-all-terms t1 L)
   (map-terms (lambda (term) (*term t1 term)) L))
 
+;;;   RepTerms --> RepTerms
+(define (negate-terms L)
+  (map-terms (lambda (term)
+               (make-term (order term) 
+                          (negate (coeff term))))
+             L))
 
 
 ;;; Procedures for Representing Term Lists.
@@ -335,6 +424,13 @@
 ;;;   RepTerms --> Bool
 (define (empty-termlist? term-list) (null? term-list))
 
+;;; mapping RepTerms
+(define (map-terms proc term-list)
+  (if (empty-termlist? term-list)
+      term-list
+      (adjoin-term (proc (first-term term-list))
+                   (map-terms proc (rest-terms term-list)))))
+
 
 ;;;  THE TERM ARITHMETIC PACKAGE
 
@@ -349,11 +445,80 @@
 ;;;   RepTerm --> GN
 (define (coeff term) (cadr term))
 
-                   ;;;   (RepTerm, RepTerm) --> RepTerm
+;;;   (RepTerm, RepTerm) --> RepTerm
 (define (*term t1 t2)
   (make-term
    (+ (order t1) (order t2))
    (mul (coeff t1) (coeff t2))))
+
+
+;;; Coercion procedure from polynomial/polynomial method
+;;; to number/polynomial method
+
+(define (repnum->reppoly variable num)
+  (make-poly variable (list (list 0 (create-number num)))))
+
+;;;  ((RepPoly, RepPoly) --> T) --> ((RepNum, RepPoly) --> T)
+(define (PPmethod->NPmethod method)
+  (lambda (num poly)
+    (method
+     (repnum->reppoly (variable poly) num)
+     poly)))
+
+;;;  ((RepPoly, RepPoly) --> T) --> ((RepPoly, RepNum) --> T)
+(define (PPmethod->PNmethod method)
+  (lambda (poly num)
+    (method
+     poly
+     (repnum->reppoly (variable poly) num))))
+
+;;; Methods for numbers and polynomials.
+
+;;;   (RepNum, RepPoly) --> ({polynomial} X RepPoly)
+(define +number-polynomial (PPmethod->NPmethod +polynomial))
+(define -number-polynomial (PPmethod->NPmethod -polynomial))
+(define *number-polynomial (PPmethod->NPmethod *polynomial))
+
+;;;   (RepPoly, RepNum) --> ({polynomial} X RepPoly)
+(define +polynomial-number (PPmethod->PNmethod +polynomial))
+(define -polynomial-number (PPmethod->PNmethod -polynomial))
+(define *polynomial-number (PPmethod->PNmethod *polynomial))
+
+(define (/polynomial-number p num)
+  (make-polynomial (/poly p (create-number num))))
+
+(define (/poly p num)
+  (make-poly (variable p)
+             (map-terms 
+               (lambda (term) (/term term num))
+               (term-list p))))
+
+(define (/term t num)
+  (make-term
+   (order t)
+   (div (coeff t) num)))
+
+;;;   (RepNum, RepPoly) --> Bool
+(define =number-polynomial (PPmethod->NPmethod equ-polynomial?))
+
+;;;   (RepPoly, RepNum) --> Bool
+(define =polynomial-number (PPmethod->PNmethod equ-polynomial?))
+
+
+;;; Install the number/polynomial methods in the generic operations.
+
+(put 'add '(number polynomial) +number-polynomial)
+(put 'sub '(number polynomial) -number-polynomial)
+(put 'mul '(number polynomial) *number-polynomial)
+
+(put 'add '(polynomial number) +polynomial-number)
+(put 'sub '(polynomial number) -polynomial-number)
+(put 'mul '(polynomial number) *polynomial-number)
+(put 'div '(polynomial number) /polynomial-number)
+
+(put 'equ? '(number polynomial) =number-polynomial)
+
+(put 'equ? '(polynomial number) =polynomial-number)
 
 
 ;;;APPLYING POLYNOMIALS
@@ -367,21 +532,13 @@
       (create-number 1)
       (mul gn (power gn (dec k)))))
 
+(define (apply-terms terms gn)
+  (if (empty-termlist? terms)
+      (create-number 0)
+      (add (apply-term (first-term terms) gn)
+           (apply-terms (rest-terms terms) gn))))
+
 (define (apply-polynomial p gn)
   (apply-terms
    (term-list (contents p))
    gn))
-
-
-
-
-
-
-
-
-
-
-
-
-
-
